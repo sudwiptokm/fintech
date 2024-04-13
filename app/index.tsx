a80a62725364fe067de10cd029abba9914ce5120
sudwiptokm <sudwiptokmondal@gmail.com>
2024-04-13 23:55:17 +0600

    fintech

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+type Props = {};
+
+const Page = (props: Props) => {
+  return (
+    <View>
+      <Text>Page</Text>
+    </View>
+  );
+};
+
+export default Page;
